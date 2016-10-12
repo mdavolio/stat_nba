a33cc71a79d49bf8a0c53c582330a327e8be9e52
Matt <md3es@virginia.edu>
2016-10-12 13:40:49 -0400

    expand table to each player on indv row

--- nba_code.R
+++ nba_code.R
@@ -2,4 +2,17 @@
 
 library(readr)
 
-all_nba <- read_csv()
+all_nba <- read_csv('all_pro.csv')
+
+all_nba_2 <- data.frame()
+count = 1
+
+for(i in 1:nrow(all_nba)){
+  for(k in 4:8){
+    all_nba_2[count,1] <- all_nba[i, 1]    
+    all_nba_2[count,2] <- all_nba[i, 2]
+    all_nba_2[count,3] <- all_nba[i, 3]
+    all_nba_2[count, 4] <- all_nba[i,k] 
+    count = count + 1
+  }
+}
