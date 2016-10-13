e568cd0caf86f7fb0b22696123a1907e8b1212b0
Matt <md3es@virginia.edu>
2016-10-13 12:14:47 -0400

    comments

--- nba_code.R
+++ nba_code.R
@@ -3,11 +3,13 @@
 library(readr)
 library(stringr)
 
+# import csv file
 all_nba <- read_csv('all_pro.csv')
 
 all_nba_2 <- data.frame()
 count = 1
 
+# change data frame so each player is individual row
 for(i in 1:nrow(all_nba)){
   for(k in 4:8){
     all_nba_2[count,1] <- all_nba[i, 1]    
@@ -18,9 +20,14 @@ for(i in 1:nrow(all_nba)){
   }
 }
 
+# split names and pos into individual columns
+# Still need to clean names that appear in position column
 all_nba_2[,4:6] <- as.data.frame(str_split_fixed(all_nba_2$V4, " ", 3))
 
+# Rename columns
 colnames(all_nba_2) <- c('Year','del','Team','First','Last','Pos')
 
+# Remove 'nba' column that was unnecessary
 drops <- ('del')
 all_nba_2 <- all_nba_2[,!(names(all_nba_2) %in% drops)]
+
