d9fa7eb7789c9c8d27791d14356f3ad449602ab8
Matt <md3es@virginia.edu>
2016-11-15 09:55:31 -0500

    Fix name column

---
+++ draft_code.R
@@ -0,0 +1,17 @@
+# Stat Final Project
+# Draft Data
+
+library(readr)
+library(stringr)
+
+# import csv file
+draft <- as.data.frame(read_csv('nbaDraftList.csv'))
+
+# Rename columns
+colnames(draft) <- c('Draft_Pos','Team','Name','College','Year')
+
+# reconfigure name column
+draft[,6:7] <- as.data.frame(str_split_fixed(draft$Name, ",", 2))
+draft$Name <- paste(draft$V2, draft$V1)
+drops <- c('V1','V2')
+draft <- draft[,!(names(draft) %in% drops)]
