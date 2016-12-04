252fc8c2110aa4a78559388d1c483203ccea0a66
Matt <md3es@virginia.edu>
2016-12-04 18:11:25 -0500

    ready for submission

--- all_nba_merging.R
+++ all_nba_merging.R
@@ -1,8 +1,11 @@
 # Stat Final Project
-# All-NBA Merging and Analysis
+# All-NBA Merging
 
 draft_box <- merge(box_scores, draft, by = c('Name'), all.x = T)
 all_nba_analysis <- merge(draft_box, all_nba_final, by = c('Name','Year'), all.x=T)
+
+# Change undrafted players draft_pos from NA to 61
+all_nba_analysis$Draft_Pos[is.na(all_nba_analysis$Draft_Pos)] <- 61
 
 # remove double columns
 doubles <- c('Pos.y','Team.x')
@@ -18,4 +21,3 @@ all_nba_analysis$All_NBA_Team[all_nba_analysis$All_NBA_Team == '1st'] <- 1
 all_nba_analysis$All_NBA_Team[all_nba_analysis$All_NBA_Team == '2nd'] <- 2
 all_nba_analysis$All_NBA_Team[all_nba_analysis$All_NBA_Team == '3rd'] <- 3
 all_nba_analysis$All_NBA_Team[is.na(all_nba_analysis$All_NBA_Team)] <- 4
-
